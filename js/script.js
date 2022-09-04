d99425cc73f9677c0eebec765814db09d02d3731
Vakhtang Tivadze <vaxotivadze@gmail.com>
2022-09-04 14:15:36 +0400

    Realize functionality to move next step while recording data

--- js/script.js
+++ js/script.js
@@ -1,10 +1,16 @@
 const employeeData = document.getElementById('employee-data');
 const laptopData = document.getElementById('laptop-data');
 const backwardButton = document.getElementById('backward-button');
+const forwardButton = document.getElementById('forward-button');
 
 window.addEventListener('load', () => {
+  forwardButton.addEventListener('click', () => {
+    employeeData.classList.add('record-groups__item--hidden');
+    laptopData.classList.remove('record-groups__item--hidden');
+  });
+
   backwardButton.addEventListener('click', () => {
     laptopData.classList.add('record-groups__item--hidden');
     employeeData.classList.remove('record-groups__item--hidden');
   });
-})
+});
